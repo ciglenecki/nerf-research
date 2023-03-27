802afee402abd83e739eb89714fe4a86f723d7cd
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2023-03-27 13:37:12 +0000

    add requreiments update and generate split trian test indices json

--- src/generate_split_train_test.py
+++ src/generate_split_train_test.py
@@ -1,12 +1,13 @@
 """
 TODO matej, finish this implementation
 """
-
 import argparse
+import json
+import math
 from enum import Enum
 from pathlib import Path
 
-from nerfstudio.nerfstudio.defaults import SPLIT_INDICES_PATH
+import numpy as np
 
 
 class Method(Enum):
@@ -29,13 +30,13 @@ def parse_args():
         "--train-size",
         metavar="float",
         type=float,
-        help="Between zero and one",
+        help="Fraction or int",
     )
     args.add_argument(
         "--val-size",
         metavar="float",
         type=float,
-        help="Between zero and one",
+        help="Fraction or int",
     )
     args.add_argument(
         "--trim",
@@ -51,7 +52,7 @@ def parse_args():
         type=Method,
         choices=Method,
         help="Subsampling method",
-        default=Method.DISPERSE_LOOP,
+        default=[Method.DISPERSE_LOOP],
     )
 
     return args.parse_known_args()
@@ -62,18 +63,66 @@ def main():
     args, unknown_args = parse_args()
     dataset_dir = args.dataset_dir
     train_size, val_size = args.train_size, args.val_size
-     
-    
+
     images_dir = Path(dataset_dir, "images")
-    image_filenames = (
-        p.resolve()
+    image_filenames = [
+        p.relative_to(images_dir).as_posix()
         for p in Path(images_dir).glob("**/*")
         if p.suffix in {".png", ".jpeg", ".jpg"}
-    )
+    ]
+    image_filenames = list(sorted(image_filenames))
 
     sequence_size = len(image_filenames)
-    if args.train_size < 1:
-        
+    trim_start, trim_end = args.trim or (0, 0)
+    trim_start = int(trim_start * sequence_size)
+    trim_end = int(trim_end * sequence_size)
+    image_filenames = image_filenames[trim_start : sequence_size - trim_end]
+
+    zfill_size = len(str(len(image_filenames)))
+    zfill_size = 2 if zfill_size == 1 else zfill_size
+
+    if val_size < 1:
+        val_size = int(val_size * len(image_filenames))
+    else:
+        val_size = int(val_size)
+
+    if train_size < 1:
+        train_size = int(train_size * len(image_filenames))
+    else:
+        train_size = int(train_size)
+
+    for method in args.methods:
+        if method == Method.DISPERSE:
+            linspace_num = val_size
+        elif method == Method.DISPERSE_LOOP:
+            linspace_num = val_size + 1
+
+        val_indices = np.rint(
+            np.linspace(0, len(image_filenames) - 1, linspace_num)
+        ).astype(int)[:-1]
+
+        train_indices = np.setdiff1d(np.arange(len(image_filenames)), val_indices)
+        train_mask = np.rint(np.linspace(0, len(train_indices) - 1, train_size)).astype(
+            int
+        )
+        train_indices = train_indices[train_mask]
+
+        # Create the indices dictionary
+        indices = {}
+        for idx in val_indices:
+            indices[image_filenames[idx]] = "val"
+        for idx in train_indices:
+            indices[image_filenames[idx]] = "train"
+
+        # Write indices.json
+
+        indices_name = f"train_{str(train_size).zfill(zfill_size)}_val_{str(val_size).zfill(zfill_size)}.json"
+        indices_path = Path(dataset_dir, indices_name)
+        with indices_path.open("w") as f:
+            json.dump(indices, f, indent=4)
+
+        print(f"Generated {indices_path}")
+
 
 if __name__ == "__main__":
 
